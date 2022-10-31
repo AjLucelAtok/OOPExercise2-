1fb31fbc7766289ba5cab2ca0a886f50dfcea6bc
AjLucelAtok <116326156+AjLucelAtok@users.noreply.github.com>
2022-11-01 00:10:15 +0800

    Update superclass.java

--- superclass.java
+++ superclass.java
@@ -1,20 +1,25 @@
-import java.util.*;
-
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World!");
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+public abstract class Account {
+	protected String name;
+	protected String id;
+	
+	public Account(String name, String id) {
+		this.name=name;
+		this.id=id;
+	}
+	
+	public abstract String ForgotPass();
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id=id;
 	}
 }
