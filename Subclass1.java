ad2b8b0ac421d8e0f3f35eab3bc08632c26065cb
AjLucelAtok <116326156+AjLucelAtok@users.noreply.github.com>
2022-11-01 00:08:12 +0800

    Update Subclass1.java

--- Subclass1.java
+++ Subclass1.java
@@ -1,3 +1,20 @@
-public class BicycleJava
-{
+public class subaccount_1 extends Account {
+	private String password;
+	
+	public subaccount_1(String name, String id, String password) {
+		super(name,id);
+		this.password=password;
+	}
+
+@Override
+public String ForgotPass() {
+	return "Did you forgot your Passowrd?\nThis is your accout details please remember:\n";
 }
+
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password=password;
+}
+} 
