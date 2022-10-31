1888e42917a798b8b9d59f09aede8df52632dc56
AjLucelAtok <116326156+AjLucelAtok@users.noreply.github.com>
2022-11-01 00:06:36 +0800

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -1,8 +1,8 @@
 public class MainProgram {
 	public static void main(String[] args) {
 		Account acc;
-		subaccount_1 acc_1= new subaccount_1("Leo Jorquia","21100447","jo1234");
-		subaccount_2 acc_2= new subaccount_2("Tyrone Dagalea","20120022","ty233");
+		subaccount_1 acc_1= new subaccount_1("AJ Lucel M. Atok","21100447","jo1234");
+		subaccount_2 acc_2= new subaccount_2("Aljen Marie Atok","20120022","ty233");
 		acc = acc_1;
 		System.out.println(acc.ForgotPass());
 		System.out.println("This is your first account.");
